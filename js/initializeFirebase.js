586a9fa5e694d4a4bc307aa5d898e3a39730d15d
L. Wood <lucaswoodbusiness@gmail.com>
2019-11-28 10:10:41 -0800

    Refactoring and maintenance

--- js/initializeFirebase.js
+++ js/initializeFirebase.js
@@ -1,5 +1,5 @@
 // Your web app's Firebase configuration
-var firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyBDCUXt6SdNeEzp0OrYkityF5yAizNrnSw",
     authDomain: "onestop-vancouver.firebaseapp.com",
     databaseURL: "https://onestop-vancouver.firebaseio.com",
