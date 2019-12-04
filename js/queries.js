3c598256ef6a133fba0b62a5b99e5e8294bb453c
kanmatthew <kanmatthew@hotmail.com>
2019-12-04 15:51:38 -0800

    Made a change to meet technical implementation requirement

--- js/queries.js
+++ js/queries.js
@@ -1,5 +1,5 @@
 function queryProducts(collection_name, user) {
-    db.collection("onestop-van").doc('stock').collection(collection_name).get().then(function (querySnapshot) {
+    db.collection("onestop-van").doc('stock').collection(collection_name).orderBy('price').get().then(function (querySnapshot) {
         querySnapshot.forEach(function (doc) {
             // doc.data() is never undefined for query doc snapshots
             let image = document.createElement("img");
