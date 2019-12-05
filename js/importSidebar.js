72307b5dc236c2d29bff9efff0f4111e1265b0d1
L. Wood <lucaswoodbusiness@gmail.com>
2019-12-05 09:35:51 -0800

    Made the importable sidebar look nicer in code

--- js/importSidebar.js
+++ js/importSidebar.js
@@ -3,50 +3,51 @@
 // 2. Include this script.
 
 function importSidebar() {
-    let sidebarHtml = '<nav id="sidebar">' +
-        '            <div class="sidebar-header">' +
-        '                <h3>OneStop Vancouver</h3>' +
-        '            </div>' +
-        '            <ul class="list-unstyled components mainMenu toplevel">' +
-        '                <li>' +
-        '                    <a href="index.html" class="toplevel">Home</a>' +
-        '                </li>' +
-        '                <li>' +
-        '                    <a href="#shoppingSubmenu" data-toggle="collapse"' +
-        '                       aria-expanded="false" class="dropdown-toggle toplevel">Shopping</a>' +
-        '                    <ul id="shoppingSubmenu" class="collapse list-unstyled">' +
-        '                        <li class="secondlevel">' +
-        '                            <a href="#categoriesSubmenu" data-toggle="collapse"' +
-        '                            aria-expanded="true" class="dropdown-toggle">Categories</a>' +
-        '                            <ul id="categoriesSubmenu" class="collapse list-unstyled toplevel">' +
-        '                                <li><a href="#" onclick="goToCategory(\'tops\');" class="toplevel">Tops</a></li>' +
-        '                                <li><a href="#" onclick="goToCategory(\'bottoms\');" class="toplevel">Bottoms</a></li>' +
-        '                                <li><a href="#" onclick="goToCategory(\'outer-wear\');" class="toplevel">Outer Wear</a></li>' +
-        '                                <li><a href="#" onclick="goToCategory(\'headwear\');" class="toplevel">Headwear</a></li>' +
-        '                                <li><a href="#" onclick="goToCategory(\'footwear\');" class="toplevel">Footwear</a></li>' +
-        '                                <li><a href="#" onclick="goToCategory(\'accessories\');" class="toplevel">Accessories</a></li>' +
-        '                            </ul>' +
-        '                        </li>   ' +
-        '                        <li class="secondlevel">' +
-        '                            <a href="WIP.html">Listings by Price</a>' +
-        '                        </li>' +
-        '                    </ul>' +
-        '                </li>' +
-        '                <li>' +
-        '                    <a href="map.html" class="toplevel">Locations</a>' +
-        '                </li>' +
-        '                <li>' +
-        '                    <a href="login.html" id = "login" title="fajardo" class="toplevel">' +
-        '                        Log In / Sign Up' +
-        '                    </a>' +
-        '                </li>' +
-        '                <li>' +
-        '                    <a href="shoppingcart.html" id = "cart" class="toplevel">' +
-        '                        Shopping Cart' +
-        '                    </a>' +
-        '                </li>' +
-        '            </ul>' +
-        '        </nav>';
+    let sidebarHtml =
+        '<nav id="sidebar">' +
+        '    <div class="sidebar-header">' +
+        '         <h3>OneStop Vancouver</h3>' +
+        '    </div>' +
+        '    <ul class="list-unstyled components mainMenu toplevel">' +
+        '    <li>' +
+        '        <a href="index.html" class="toplevel">Home</a>' +
+        '    </li>' +
+        '    <li>' +
+        '        <a href="#shoppingSubmenu" data-toggle="collapse"' +
+        '           aria-expanded="false" class="dropdown-toggle toplevel">Shopping</a>' +
+        '        <ul id="shoppingSubmenu" class="collapse list-unstyled">' +
+        '            <li class="secondlevel">' +
+        '                <a href="#categoriesSubmenu" data-toggle="collapse"' +
+        '                aria-expanded="true" class="dropdown-toggle">Categories</a>' +
+        '                <ul id="categoriesSubmenu" class="collapse list-unstyled toplevel">' +
+        '                    <li><a href="#" onclick="goToCategory(\'tops\');" class="toplevel">Tops</a></li>' +
+        '                    <li><a href="#" onclick="goToCategory(\'bottoms\');" class="toplevel">Bottoms</a></li>' +
+        '                    <li><a href="#" onclick="goToCategory(\'outer-wear\');" class="toplevel">Outer Wear</a></li>' +
+        '                    <li><a href="#" onclick="goToCategory(\'headwear\');" class="toplevel">Headwear</a></li>' +
+        '                    <li><a href="#" onclick="goToCategory(\'footwear\');" class="toplevel">Footwear</a></li>' +
+        '                    <li><a href="#" onclick="goToCategory(\'accessories\');" class="toplevel">Accessories</a></li>' +
+        '                </ul>' +
+        '            </li>   ' +
+        '            <li class="secondlevel">' +
+        '                <a href="WIP.html">Listings by Price</a>' +
+        '            </li>' +
+        '        </ul>' +
+        '    </li>' +
+        '    <li>' +
+        '        <a href="map.html" class="toplevel">Locations</a>' +
+        '    </li>' +
+        '    <li>' +
+        '        <a href="login.html" id = "login" title="fajardo" class="toplevel">' +
+        '            Log In / Sign Up' +
+        '        </a>' +
+        '    </li>' +
+        '    <li>' +
+        '        <a href="shoppingcart.html" id = "cart" class="toplevel">' +
+        '            Shopping Cart' +
+        '        </a>' +
+        '    </li>' +
+        '    </ul>' +
+        '</nav>';
     let sidebarLink = document.getElementById('sidebar');
     sidebarLink.outerHTML = sidebarHtml;
 }
